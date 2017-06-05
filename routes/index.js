62236a6280e717e1f6b5cc907c106ff8a2193853
Marissa Monivis <mmonivis@gmail.com>
2017-06-05 15:11:59 -0400

    Added basic screen structure and connected MySQL to the webpage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+// Import our custom "config" node module.
+// It holds a single object that has our MySQL credentials.
+var config = require('../config/config');
+// Include the MySQL module. We got this via npm
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: config.host,
+	user: config.userName,
+	password: config.password,
+	database: config.database
+});
+
+// We are now connected (after this line!)
+connection.connect();
+// now we can write some awesome querys
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Add a post route "addItem" to handle the form submission.
+router.post('/addItem',(req,res)=>{
+	// res.json(req.body) // Helpful for us to visualize what we
+	var newTask = req.body.newTask;
+	var dueDate = req.body.newTaskDate;
+	// We know what they submitted from the form. It comes to this route inside
+	// req.body.NAMEOFFIELD. Now we need to insert it into MySQL.
+	var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES('"+newTask+"','"+dueDate+"')";
+	// res.send(insertQuery);
+	connection.query(insertQuery,(error,results)=>{
+		if(error) throw error;
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
